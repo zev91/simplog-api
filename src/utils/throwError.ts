7d3efab35dd18c67bbb69f7774be1c6fcce8c0b7
zhuwei@xhqb.com <zhuwei@xhqb.com>
2020-04-26 14:51:42 +0800

    评论接口排序

--- src/utils/throwError.ts
+++ src/utils/throwError.ts
@@ -4,3 +4,7 @@ import { NOT_FOUND } from "http-status-codes";
 export const throwPostNotFound = () => {
   throw new HttpException(NOT_FOUND, '请求不存在 !');
 }
+
+export const throwCommentNotFound = () => {
+  throw new HttpException(NOT_FOUND, '请求不存在 !');
+}
